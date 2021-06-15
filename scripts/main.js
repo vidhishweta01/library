f29c567d065a2bbde3ea4ce83e608968bd2aeea8
vidhishweta01 <vidhishweta01@gmail.com>
2021-06-15 15:51:52 +0530

    added css background and script

---
+++ scripts/main.js
@@ -0,0 +1,87 @@
+const myLibrary = [];
+const submitButton = document.querySelector('.submit-btn');
+const form = document.querySelector('form');
+const addBook = document.querySelector('.add-book');
+
+// FUNCTIONS
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+function deleteBook() {
+  myLibrary.splice(this.parentNode, 1);
+  this.parentNode.remove();
+}
+
+function display() {
+  const container = document.querySelector('.container');
+  container.innerHTML = '';
+
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+
+    const title = document.createElement('h3');
+    title.classList.add('book-title');
+    title.textContent = book.title;
+
+    const author = document.createElement('p');
+    author.classList.add('book-author');
+    author.textContent = `Author: ${book.author}`;
+
+    const pages = document.createElement('p');
+    pages.classList.add('book-pages');
+    pages.textContent = `Pages: ${book.pages}`;
+
+    const read = document.createElement('button');
+    read.classList.add('book-read');
+    read.textContent = book.read === 'Not yet read' ? 'Not yet read' : 'Finished reading.';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.textContent = 'Delete';
+
+    bookDiv.append(title, author, pages, read, deleteButton);
+    container.appendChild(bookDiv);
+
+    read.addEventListener('click', (e) => {
+      book.read = book.read === 'Not yet read'
+        ? 'Finished reading'
+        : 'Not yet read';
+      e.target.textContent = e.target.textContent === 'Not yet read'
+        ? 'Finished reading'
+        : 'Not yet read';
+    });
+
+    deleteButton.addEventListener('click', deleteBook);
+  });
+}
+
+// EVENT LISTENERS
+
+
+submitButton.addEventListener('click', (bookData) => {
+  bookData.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  pages = parseInt(pages);
+  let read = '';
+  if (document.getElementById('Not yet read').checked) {
+    read = document.getElementById('Not yet read').value;
+  } else {
+    read = document.getElementById('Finished reading').value;
+  }
+  addBookToLibrary(title, author, pages, read);
+  form.reset();
+  display();
+});
